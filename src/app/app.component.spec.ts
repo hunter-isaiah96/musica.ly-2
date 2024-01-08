db2053e34aa2ba8296678e1a7bc7e22636150f3f
Isaiah Hunter <hunter.isaiah96@gmail.com>
2024-01-08 16:32:36 -0500

    testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,25 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { provideNoopAnimations } from '@angular/platform-browser/animations';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NgxsModule, Store } from '@ngxs/store';
+import { importProvidersFrom } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AudioPlayerState } from './store/audioplayer/audio-player.state';
 
 describe('AppComponent', () => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
-
+  let store: Store;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      providers: [provideNoopAnimations()],
-      imports: [AppComponent],
+      providers: [
+        provideNoopAnimations(),
+        importProvidersFrom(NgxsModule.forRoot([AudioPlayerState])),
+      ],
+      imports: [AppComponent, RouterTestingModule, HttpClientTestingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
+    store = TestBed.inject(Store);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-
-  it('Menu button toggles the sidenav', () => {});
 });
--- src/app/pages/track-info/track-info.component.spec.ts
+++ src/app/pages/track-info/track-info.component.spec.ts
@@ -2,9 +2,10 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TrackInfoComponent } from './track-info.component';
 import { AudioPlayerService } from '../../services/audio-player/audio-player.service';
-import { Store } from '@ngxs/store';
+import { NgxsModule, Store } from '@ngxs/store';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { importProvidersFrom } from '@angular/core';
 
 describe('TrackInfoComponent', () => {
   let component: TrackInfoComponent;
@@ -18,10 +19,13 @@ describe('TrackInfoComponent', () => {
         HttpClientTestingModule,
         RouterTestingModule,
       ],
+      providers: [importProvidersFrom(NgxsModule.forRoot([]))],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TrackInfoComponent);
     component = fixture.componentInstance;
+    audioService = TestBed.inject(AudioPlayerService);
+    store = TestBed.inject(Store);
     fixture.detectChanges();
   });
 
